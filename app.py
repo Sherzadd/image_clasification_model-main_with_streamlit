b20017860a6831ef225fba9ea35b10c586f76f61
Sherzadoo <sherzadzabihullahg@gmail.com>
2026-01-11 03:59:16 +0100

    title

--- app.py
+++ app.py
@@ -199,9 +199,9 @@ with right:
     st.markdown(
         """
 <div style="display:flex; align-items:flex-start; gap:0.75rem;">
-  <div style="font-size:2.6rem; line-height:1;">🌿</div>
+  <div style="font-size:2.6rem; line-height:1;"/div>
   <div style="font-size:2.6rem; font-weight:700; line-height:1.08;">
-    Plant Disease identification with AI
+    Plant Disease identification with AI >🌿<
   </div>
 </div>
 """,
